9fc83d936ad23d8d4a3733c75a744faf2c650da1
Simon Redman <simon@ergotech.com>
2018-05-16 19:44:45 -0600

    After all that effort, move ConversationModel and ConversationListModel back to smsapp since they are about to depend on KPeople

--- smsapp/main.cpp
+++ smsapp/main.cpp
@@ -18,8 +18,8 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-#include "interfaces/conversationmodel.h"
-#include "interfaces/conversationlistmodel.h"
+#include "conversationmodel.h"
+#include "conversationlistmodel.h"
 #include "kdeconnect-version.h"
 
 #include <QApplication>
